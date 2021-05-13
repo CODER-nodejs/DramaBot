1d2bcdd816a5c2d800fc2ecc67ca3ebfee8644c6
CODER-nodejs <77936905+CODER-nodejs@users.noreply.github.com>
2021-05-13 14:03:13 +0200

    Update dramagen.js

--- commands/dramagen.js
+++ commands/dramagen.js
@@ -1,7 +1,30 @@
 module.exports = {
     name: 'dramagen',
     description: 'This sends drama',
-   async execute(message, args){
-        
+   async execute(client, Discord, rp, cheerio, message, args){
+        const URL = "https://drama.gg/generator/api/";
+     const headerObj = {
+       uri: URL
+     }
+
+
+     rp(headerObj)
+     .then((html)=>{
+       var $ = cheerio.load(html)
+       const header = $('body')
+       .text()
+       .trim();
+      const drama = new Discord.MessageEmbed()
+      .setColor('#55ff55')
+      .setTitle('Drama generator')
+      .addFields(
+        {name: 'Something coming...', value: header}
+      )
+      message.channel.send(drama)
+     }).catch((err)=>{
+
+      console.log(err)
+
+     })
     }
 }
